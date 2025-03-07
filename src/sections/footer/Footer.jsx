3996968949119433d129e0ed808deb41510dcaa8
Daniella Bosc <daniellabosc@gmail.com>
2025-03-07 11:56:57 -0700

    wtf

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,11 +1,9 @@
-import ContactBar from '../../components/contactbar/ContactBar'
 import NavBar from '../../components/navbar/NavBar'
 
 const Footer = () => {
   return (
     <header>
       <NavBar position='bottom' />
-      <ContactBar />
     </header>
   )
 }
