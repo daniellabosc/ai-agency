65504735127f95890d24855646f4fad2d576e4b2
Daniella Bosc <daniellabosc@gmail.com>
2025-03-06 20:33:47 -0700

    no socials

--- src/components/contactbar/ContactBar.jsx
+++ src/components/contactbar/ContactBar.jsx
@@ -7,37 +7,6 @@ import LinkedIn from "../../assets/linkedin.png";
 const ContactBar = () => {
   return (
     <Row className="contact-bar__main">
-      <Col lg={6} sm={12}>
-        <Row className="contact-bar__contact">
-          <Col xs={12} lg={12}>
-            <span>
-              <a href="mailto:malgorzata@axion-studio.com">
-                Contact Us
-              </a>
-            </span>
-          </Col>
-        </Row>
-      </Col>
-      {/* <h1>Healthy Pets Design - Web Design and Development</h1> */}
-      <Col lg={6} sm={12}>
-        <div className="contact-bar__socials">
-          <a
-            href="https://www.facebook.com/profile.php?id=100095594951319"
-            target="_new"
-          >
-            <img src={FaceBook} alt="facebook icon" className="icon" />
-          </a>
-          <a href="https://www.instagram.com/healthypets.design/" target="_new">
-            <img src={Instagram} alt="instagram icon" className="icon" />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/healthypetsdesign/?viewAsMember=true"
-            target="_new"
-          >
-            <img src={LinkedIn} alt="linkedin icon" className="icon" />
-          </a>
-        </div>
-      </Col>
     </Row>
   );
 };
