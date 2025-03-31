454cb9ce7e85927ac0ab72eab2e355f8d1a82020
Daniella Bosc <daniellabosc@gmail.com>
2025-03-30 19:47:46 -0700

    changes

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,6 @@
 import "./contact.css";
 import { Container, Row, Col } from "react-bootstrap";
 import ContactForm from "../../components/contactform/ContactForm";
-//import Logo from "../../assets/logo.png";
-import BookLink from "../../components/booklink/Booklink";
 
 const Contact = () => {
   return (
@@ -13,10 +11,6 @@ const Contact = () => {
             <Col lg={12} sm={12}>
               <ContactForm />
             </Col>
-            {/* <Col lg={12} sm={12}>
-              <BookLink />
-            </Col> */}
-            {/* <Nav.Link href="#top" className="top_btn"><img src={Logo} alt="logo" className="btn-logo btn-optional" /></Nav.Link> */}
           </Row>
         </Container>
       </div>
